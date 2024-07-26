cff43991570436666d3aca9170352284e5001ef1
Stanislav.Koloncev <stas.koloncev232@gmail.com>
2024-07-26 20:08:48 +0200

    error correction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
