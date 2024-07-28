bb0b80b1874304076857cfb0b284a5b104debed2
Stanislav.Koloncev <stas.koloncev232@gmail.com>
2024-07-28 04:10:46 +0200

    newStyle

--- app/src/main/java/com/example/criminalmove/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalmove/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -31,8 +32,7 @@ class CrimeListFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
 
-        crimeRecyclerView =
-            view.findViewById(R.id.crime_recycler_view) as RecyclerView
+        crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
         updateUI()
@@ -46,30 +46,33 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView.adapter = adapter
     }
 
-    private inner class CrimeHolder(view: View)
-        : RecyclerView.ViewHolder(view){
-            val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
-            val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
-        }
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        val solvedImageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
 
-    private inner class CrimeAdapter(var crimes: List<Crime>)
-        : RecyclerView.Adapter<CrimeHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-                : CrimeHolder {
+    private inner class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
             val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
             return CrimeHolder(view)
         }
+
         override fun getItemCount() = crimes.size
+
         override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
             val crime = crimes[position]
             holder.apply {
-                titleTextView.text =crime.title
+                titleTextView.text = crime.title
                 dateTextView.text = crime.date.toString()
+                solvedImageView.visibility = if (crime.isSolved) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
             }
-
         }
-
-        }
+    }
 
     companion object {
         fun newInstance(): CrimeListFragment {
@@ -77,4 +80,3 @@ class CrimeListFragment : Fragment() {
         }
     }
 }
-
